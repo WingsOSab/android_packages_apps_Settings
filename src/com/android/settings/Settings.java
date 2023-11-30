a20be92cc24624a17565358ee0a9e1f873de5072
maxwen <max.weninger@gmail.com>
2023-11-30 07:17:44 +0100

    Settings: Add `WingsExtension` to dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -485,4 +485,7 @@ public class Settings extends SettingsActivity {
      * Activity for PreviouslyConnectedDeviceDashboardFragment
      */
     public static class PreviouslyConnectedDevicesActivity extends SettingsActivity { /* empty */ }
+
+    // Wings Extension Additoon
+    public static class WingsExtensionActivity extends SettingsActivity {}
 }
