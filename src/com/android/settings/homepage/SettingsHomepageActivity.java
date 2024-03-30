e6c0bbaca2622e65e804b712cf51bb8423ed1124
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-31 07:36:07 +0800

    Settings: Add avatar to settings homepage
    * reference used:
    https://github.com/crdroidandroid/android_packages_apps_Settings/commit/b7fc168da378d738d074eed96e9316d10552cbca
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -85,6 +85,8 @@ import com.android.settingslib.core.lifecycle.HideNonSystemOverlayMixin;
 
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
+import com.android.settings.utils.UserUtils;
+
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Set;
@@ -127,6 +129,8 @@ public class SettingsHomepageActivity extends FragmentActivity implements
     // A regular layout shows icons on homepage, whereas a simplified layout doesn't.
     private boolean mIsRegularLayout = true;
     CollapsingToolbarLayout collapsing_toolbar;
+
+    private UserUtils mUserUtils;
 
     private SplitControllerCallbackAdapter mSplitControllerAdapter;
     private SplitInfoCallback mCallback;
@@ -232,7 +236,11 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         updateHomepageBackground();
         mLoadedListeners = new ArraySet<>();
 
+        mUserUtils = UserUtils.Companion.getInstance(getApplicationContext());
+
         initSearchBarView();
+        
+        initAvatarView();
 
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
         mCategoryMixin = new CategoryMixin(this);
@@ -241,7 +249,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         final String highlightMenuKey = getHighlightMenuKey();
         // Only allow features on high ram devices.
         if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
-            initAvatarView();
             final boolean scrollNeeded = mIsEmbeddingActivityEnabled
                     && !TextUtils.equals(getString(DEFAULT_HIGHLIGHT_MENU_KEY), highlightMenuKey);
             showSuggestionFragment(scrollNeeded);
@@ -377,6 +384,12 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         enableTaskLocaleOverride();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        initAvatarView();
+    }
+
     @VisibleForTesting
     void initSplitPairRules() {
         new ActivityEmbeddingRulesController(getApplicationContext()).initRules();
@@ -488,15 +501,8 @@ public class SettingsHomepageActivity extends FragmentActivity implements
 
     private void initAvatarView() {
         final ImageView avatarView = findViewById(R.id.account_avatar);
-        final ImageView avatarTwoPaneView = findViewById(R.id.account_avatar_two_pane_version);
-        if (AvatarViewMixin.isAvatarSupported(this)) {
-            avatarView.setVisibility(View.VISIBLE);
-            getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
-
-            if (mIsEmbeddingActivityEnabled) {
-                avatarTwoPaneView.setVisibility(View.VISIBLE);
-                getLifecycle().addObserver(new AvatarViewMixin(this, avatarTwoPaneView));
-            }
+        if (avatarView != null) {
+            mUserUtils.setUserAvatarToView(avatarView);
         }
     }
 
