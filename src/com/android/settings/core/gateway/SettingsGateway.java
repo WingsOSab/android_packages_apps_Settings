SonalSingh18/evo_base

Settings: Introduce automated Sleep Mode [2/2]
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -103,6 +103,7 @@ import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.AssistGestureSettings;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
@@ -362,7 +363,8 @@ public class SettingsGateway {
             WifiTetherClientManager.class.getName(),
             RunningServices.class.getName(),
             SmartPixels.class.getName(),
-            SecurityHubDashboard.class.getName()
+            SecurityHubDashboard.class.getName(),
+            SleepMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -409,5 +411,6 @@ public class SettingsGateway {
             Settings.HeadsUpSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName(),
     };
 }

src/com/android/settings/fuelgauge/sleepmode/SleepModeReceiver.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fuelgauge.sleepmode;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.android.internal.util.evolution.EvolutionUtils.SleepModeController;
+
+public class SleepModeReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "SleepModeReceiver";
+
+    public SleepModeReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null &&
+                intent.getAction().equals(SleepModeController.SLEEP_MODE_TURN_OFF)) {
+            Settings.Secure.putInt(context.getContentResolver(), Settings.Secure.SLEEP_MODE_ENABLED, 0);
+        }
+    }
+}

WingsOSab/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -112,6 +112,7 @@ import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
 import com.android.settings.gestures.DoubleTapScreenSettings;
@@ -385,7 +386,8 @@ public class SettingsGateway {
             WingsExtension.class.getName(),
             RunningServices.class.getName(),
             LiveDisplaySettings.class.getName(),
-            VolumeSteps.class.getName()
+            VolumeSteps.class.getName(),
+            SleepMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -434,5 +436,6 @@ public class SettingsGateway {
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.WingsExtensionActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName()
+            Settings.SleepModeActivity.class.getName(),
     };
 }

src/com/android/settings/fuelgauge/sleepmode/SleepModeReceiver.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fuelgauge.sleepmode;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.android.internal.util.everest.EverestUtils.SleepModeController;
+
+public class SleepModeReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "SleepModeReceiver";
+
+    public SleepModeReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null &&
+                intent.getAction().equals(SleepModeController.SLEEP_MODE_TURN_OFF)) {
+            Settings.Secure.putInt(context.getContentResolver(), Settings.Secure.SLEEP_MODE_ENABLED, 0);
+        }
+    }
+}